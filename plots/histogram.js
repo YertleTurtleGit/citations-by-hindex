2b2dffe65927d811752eea292a24bb8dd558d7df
YertleTurtleGit <github@galzone.de>
2024-05-11 13:21:00 +0200

    d3

--- plots/histogram.js
+++ plots/histogram.js
@@ -1,20 +1,100 @@
-/* global Chart */
+/* global d3 */
 
-const plotRenderingContext = document.getElementById("plot").getContext("2d");
+const container =  document.getElementById("plot");
 
-const dataValues = [12, 19, 3, 5, 10];
-const dataLabels = ["bla", 1, 2, 3, 4];
+selfHindexList = [];
+citedByHindexList = [];
 
-const myChart = new Chart(plotRenderingContext, {
-  type: "bar",
-  data: {
-    labels: dataLabels,
-    datasets: [
-      {
-        label: "",
-        data: dataValues,
-        backgroundColor: "rgba(255, 99, 132, 1)",
-      },
-    ],
-  },
+hindices.forEach((dataPoint) => {
+  dataPoint.self.forEach((selfHindex) => {
+    dataPoint.citedBy.forEach((citedByHindex) => {
+      selfHindexList.push(selfHindex);
+      citedByHindexList.push(citedByHindex);
+    });
+  });
 });
+
+console.log(selfHindexList)
+
+const width = 960;
+const height = 500;
+const marginTop = 20;
+const marginRight = 20;
+const marginBottom = 30;
+const marginLeft = 40;
+
+// Bin the data.
+const bins = d3
+  .bin()
+  .thresholds(40)
+  .value((d) => d)(selfHindexList);
+
+// Declare the x (horizontal position) scale.
+const x = d3
+  .scaleLinear()
+  .domain([bins[0].x0, bins[bins.length - 1].x1])
+  .range([marginLeft, width - marginRight]);
+
+// Declare the y (vertical position) scale.
+const y = d3
+  .scaleLinear()
+  .domain([0, d3.max(bins, (d) => d.length)])
+  .range([height - marginBottom, marginTop]);
+
+// Create the SVG container.
+const svg = d3
+  .create("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .attr("viewBox", [0, 0, width, height])
+  .attr("style", "max-width: 100%; height: auto;");
+
+// Add a rect for each bin.
+svg
+  .append("g")
+  .attr("fill", "steelblue")
+  .selectAll()
+  .data(bins)
+  .join("rect")
+  .attr("x", (d) => x(d.x0) + 1)
+  .attr("width", (d) => x(d.x1) - x(d.x0) - 1)
+  .attr("y", (d) => y(d.length))
+  .attr("height", (d) => y(0) - y(d.length));
+
+// Add the x-axis and label.
+svg
+  .append("g")
+  .attr("transform", `translate(0,${height - marginBottom})`)
+  .call(
+    d3
+      .axisBottom(x)
+      .ticks(width / 80)
+      .tickSizeOuter(0)
+  )
+  .call((g) =>
+    g
+      .append("text")
+      .attr("x", width)
+      .attr("y", marginBottom - 4)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "end")
+      .text("Unemployment rate (%) →")
+  );
+
+// Add the y-axis and label, and remove the domain line.
+svg
+  .append("g")
+  .attr("transform", `translate(${marginLeft},0)`)
+  .call(d3.axisLeft(y).ticks(height / 40))
+  .call((g) => g.select(".domain").remove())
+  .call((g) =>
+    g
+      .append("text")
+      .attr("x", -marginLeft)
+      .attr("y", 10)
+      .attr("fill", "currentColor")
+      .attr("text-anchor", "start")
+      .text("↑ Frequency (no. of counties)")
+  );
+
+container.append(svg.node());
