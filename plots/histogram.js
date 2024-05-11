16edaf2d73e6ea21a73a66b90089023775c48372
YertleTurtleGit <github@galzone.de>
2024-05-11 16:31:04 +0200

    real data

--- plots/histogram.js
+++ plots/histogram.js
@@ -1,100 +1,106 @@
 /* global d3 */
 
-const container =  document.getElementById("plot");
+const container = document.getElementById("plot");
 
-selfHindexList = [];
-citedByHindexList = [];
+const downCite = [];
+const upCite = [];
 
 hindices.forEach((dataPoint) => {
   dataPoint.self.forEach((selfHindex) => {
     dataPoint.citedBy.forEach((citedByHindex) => {
-      selfHindexList.push(selfHindex);
-      citedByHindexList.push(citedByHindex);
+      if (selfHindex < citedByHindex) {
+        downCite.push(citedByHindex);
+      } else if (selfHindex > citedByHindex) {
+        upCite.push(citedByHindex);
+      }
     });
   });
 });
 
-console.log(selfHindexList)
+const allHindices = hindices.flatMap((obj) => [...obj.self, ...obj.citedBy]);
+const highestHindex = Math.max(...allHindices);
+const lowestHindex = Math.min(...allHindices);
 
-const width = 960;
-const height = 500;
-const marginTop = 20;
-const marginRight = 20;
-const marginBottom = 30;
-const marginLeft = 40;
+const width = window.innerWidth;
+const height = window.innerHeight / 2;
+const margin = 50;
 
-// Bin the data.
-const bins = d3
-  .bin()
-  .thresholds(40)
-  .value((d) => d)(selfHindexList);
+function drawHistogram(data, color) {
+  // Bin the data.
+  const bins = d3
+    .bin()
+    .thresholds(40)
+    .value((d) => d)(data);
 
-// Declare the x (horizontal position) scale.
-const x = d3
-  .scaleLinear()
-  .domain([bins[0].x0, bins[bins.length - 1].x1])
-  .range([marginLeft, width - marginRight]);
+  // Declare the x (horizontal position) scale.
+  const x = d3
+    .scaleLinear()
+    .domain([0, highestHindex])
+    .range([margin, width - margin]);
 
-// Declare the y (vertical position) scale.
-const y = d3
-  .scaleLinear()
-  .domain([0, d3.max(bins, (d) => d.length)])
-  .range([height - marginBottom, marginTop]);
+  // Declare the y (vertical position) scale.
+  const y = d3
+    .scaleLinear()
+    .domain([0, 200])
+    .range([height - margin, margin]);
 
-// Create the SVG container.
-const svg = d3
-  .create("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .attr("viewBox", [0, 0, width, height])
-  .attr("style", "max-width: 100%; height: auto;");
+  // Create the SVG container.
+  const svg = d3
+    .create("svg")
+    .attr("width", width)
+    .attr("height", height)
+    .attr("viewBox", [0, 0, width, height]);
 
-// Add a rect for each bin.
-svg
-  .append("g")
-  .attr("fill", "steelblue")
-  .selectAll()
-  .data(bins)
-  .join("rect")
-  .attr("x", (d) => x(d.x0) + 1)
-  .attr("width", (d) => x(d.x1) - x(d.x0) - 1)
-  .attr("y", (d) => y(d.length))
-  .attr("height", (d) => y(0) - y(d.length));
+  // Add a rect for each bin.
+  svg
+    .append("g")
+    .attr("fill", color)
+    .selectAll()
+    .data(bins)
 
-// Add the x-axis and label.
-svg
-  .append("g")
-  .attr("transform", `translate(0,${height - marginBottom})`)
-  .call(
-    d3
-      .axisBottom(x)
-      .ticks(width / 80)
-      .tickSizeOuter(0)
-  )
-  .call((g) =>
-    g
-      .append("text")
-      .attr("x", width)
-      .attr("y", marginBottom - 4)
-      .attr("fill", "currentColor")
-      .attr("text-anchor", "end")
-      .text("h-index →")
-  );
+    .join("rect")
+    .attr("x", (d) => x(d.x0) + 1)
+    .attr("width", (d) => x(d.x1) - x(d.x0) - 1)
+    .attr("y", (d) => y(d.length))
+    .attr("height", (d) => y(0) - y(d.length));
 
-// Add the y-axis and label, and remove the domain line.
-svg
-  .append("g")
-  .attr("transform", `translate(${marginLeft},0)`)
-  .call(d3.axisLeft(y).ticks(height / 40))
-  .call((g) => g.select(".domain").remove())
-  .call((g) =>
-    g
-      .append("text")
-      .attr("x", -marginLeft)
-      .attr("y", 10)
-      .attr("fill", "currentColor")
-      .attr("text-anchor", "start")
-      .text("↑ Frequency")
-  );
+  // Add the x-axis and label.
+  svg
+    .append("g")
+    .attr("transform", `translate(0,${height - margin})`)
+    .call(
+      d3
+        .axisBottom(x)
+        .ticks(width / 80)
+        .tickSizeOuter(0)
+    )
+    .call((g) =>
+      g
+        .append("text")
+        .attr("x", width)
+        .attr("y", margin - 4)
+        .attr("fill", "margin")
+        .attr("text-anchor", "end")
+        .text("h-index →")
+    );
 
-container.append(svg.node());
+  // Add the y-axis and label, and remove the domain line.
+  svg
+    .append("g")
+    .attr("transform", `translate(${margin},0)`)
+    .call(d3.axisLeft(y).ticks(height / 40))
+    .call((g) =>
+      g
+        .append("text")
+        .attr("x", -margin)
+        .attr("y", 10)
+        .attr("fill", "currentColor")
+        .attr("text-anchor", "start")
+        .text("↑ Frequency")
+    );
+
+  container.append(svg.node());
+}
+
+drawHistogram(downCite, "blue");
+drawHistogram(upCite, "red");
